39b6c7b7759ba9114fce1d78e47a5110e0c44904
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-15 21:40:09 +0545

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 
 
-{/* <div className='absolute top-0 left-0 bg-primary right-0 bottom-0 flex items-start justify-center h-screen   w-full '>
-<div className=' h-full w-10/12 flex flex-col gap-6 justify-center '>
-<h2 className='font-bold text-gray-00'>WELCOME TO ACADEMIA</h2>
-<div className='font-bold text-4xl w-4/12 text-white '>Best Online Education Expertise</div>
-<h2 className='w-5/12'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</h2>
-</div>
-</div> */}
