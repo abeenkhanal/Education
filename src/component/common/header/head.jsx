d0c832c0857e838daf87f3b4b843c76605c826a0
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-15 14:59:46 +0545

    formated

--- src/component/common/header/head.jsx
+++ src/component/common/header/head.jsx
@@ -21,27 +21,27 @@ const Head = () => {
           <MdOutlineAccessTime />
         </div>
         <div>
-        <h2>Monday-Friday</h2>
-        <h2>8:00-5:00</h2>
+          <h2>Monday-Friday</h2>
+          <h2>8:00-5:00</h2>
         </div>
       </div>
 
 
       <div className='flex justify-center'>
         <div className='text-5xl'>
-        <FiPhone />
+          <FiPhone />
         </div>
         <div>
-        <h2>Call us</h2>
-        <h2>97986758647</h2>
+          <h2>Call us</h2>
+          <h2>97986758647</h2>
         </div>
       </div>
 
       <div className='flex gap-2 items-center text-3xl '>
-      <SiFacebook className='cursor-pointer  hover:text-gray-500'/>
-      <RiInstagramFill className='cursor-pointer  hover:text-gray-500'/>
-      <FaTwitter className='cursor-pointer  hover:text-gray-500'/>
-      <FaLinkedin className='cursor-pointer  hover:text-gray-500'/>
+        <SiFacebook className='cursor-pointer  hover:text-gray-500' />
+        <RiInstagramFill className='cursor-pointer  hover:text-gray-500' />
+        <FaTwitter className='cursor-pointer  hover:text-gray-500' />
+        <FaLinkedin className='cursor-pointer  hover:text-gray-500' />
       </div>
     </div>
   )
