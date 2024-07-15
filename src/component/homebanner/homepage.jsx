aed2d3a2d31b3802b6a3068f613ede0d91e1a445
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-15 22:13:31 +0545

    hello

--- src/component/homebanner/homepage.jsx
+++ src/component/homebanner/homepage.jsx
@@ -8,8 +8,9 @@ const Homepage = () => {
             <div className=' h-full w-10/12 flex flex-col gap-6 justify-center '>
                 <h2 className='font-bold text-gray-00'>WELCOME TO ACADEMIA</h2>
                 <h1 className='font-bold text-4xl w-4/12 text-white '>Best Online Education Expertise </h1>
-                <h2 className='w-5/12'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</h2>
             </div>
+            <p className='w-5/12'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</p>
+
         </div>
     )
 }
