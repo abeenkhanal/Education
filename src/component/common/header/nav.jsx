5dde55bcdf054e9f04fe7c7ff5f1bd9e244c6a40
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-15 15:00:46 +0545

    formated

--- src/component/common/header/nav.jsx
+++ src/component/common/header/nav.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 const Nav = () => {
     return (
         <>
-        <div className='flex justify-between bg-gray-400 h-16 text-white'>
-            <ul className='flex gap-16 justify-center items-center font-bold  cursor-pointer '>
-                <li className='cursor-pointer  hover:text-gray-500'>Home</li>
-                <li className='cursor-pointer  hover:text-gray-500'>All Course</li>
-                <li className='cursor-pointer  hover:text-gray-500'>About</li>
-                <li className='cursor-pointer  hover:text-gray-500'>Team</li>
-                <li className='cursor-pointer  hover:text-gray-500'>Pricing</li>
-                <li className='cursor-pointer  hover:text-gray-500'>Journal</li>
-                <li className='cursor-pointer  hover:text-gray-500'>Contact</li>
-            </ul>
-            <div className=' bg-green-300 items-center font-semibold flex justify-center clip-polygon w-[240px] '>
-            <h1>GET CERTIFICATE</h1>
+            <div className='flex justify-between bg-gray-400 h-16 text-white'>
+                <ul className='flex gap-16 justify-center items-center font-bold  cursor-pointer '>
+                    <li className='cursor-pointer  hover:text-gray-500'>Home</li>
+                    <li className='cursor-pointer  hover:text-gray-500'>All Course</li>
+                    <li className='cursor-pointer  hover:text-gray-500'>About</li>
+                    <li className='cursor-pointer  hover:text-gray-500'>Team</li>
+                    <li className='cursor-pointer  hover:text-gray-500'>Pricing</li>
+                    <li className='cursor-pointer  hover:text-gray-500'>Journal</li>
+                    <li className='cursor-pointer  hover:text-gray-500'>Contact</li>
+                </ul>
+                <div className=' bg-green-300 items-center font-semibold flex justify-center clip-polygon w-[240px] '>
+                    <h1>GET CERTIFICATE</h1>
+                </div>
             </div>
-            </div>
-            </>
+        </>
     )
 }
 
