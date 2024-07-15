eaf4566b81b0db0740c5251710c0ba318ef92d4d
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-15 14:58:33 +0545

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Head from './component/common/header/head'
 import Nav from './component/common/header/nav'
+import footer from './component/common/footer/footer'
+import Footer from './component/common/footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,7 @@ function App() {
   return (<>
     <Head />
     <Nav />
-
+<Footer/>
     </>
   )
 }
