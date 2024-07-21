a1f759e7eed103519187498715d7bc421f4b0ba6
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-21 12:55:41 +0545

    hello

--- src/component/onlinecourse/onlinecourse.jsx
+++ src/component/onlinecourse/onlinecourse.jsx
@@ -2,66 +2,82 @@ import React from 'react'
 const Onlinecourse = () => {
   const OnlinecoursseData = [
     {
-      icon: '.hello.png',
-      title: 'jfaksbkj',
-      course: 'ksajbbakvb'
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
     },
     {
-      icon: '.hello.png',
-      title: 'jfaksbkj',
-      course: 'ksajbbakvb'
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
     },
     {
-      icon: '.hello.png',
-      title: 'jfaksbkj',
-      course: 'ksajbbakvb'
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
     },
     {
-      icon: '.hello.png',
-      title: 'jfaksbkj',
-      course: 'ksajbbakvb'
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
     },
     {
-      icon: '.hello.png',
-      title: 'jfaksbkj',
-      course: 'ksajbbakvb'
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
     },
     {
-      icon: '.hello.png',
-      title: 'jfaksbkj',
-      course: 'ksajbbakvb'
-    }
-  
-  
-  
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
+    },
+    {
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
+    },
+    {
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
+    },
+    {
+      icon: 'book.png',
+      title: 'UI/UX Design Courses',
+      course: '25 course'
+    },
+
+
 
   ]
 
 
   return (
-    <div className='bg-primary '>
-        <div>
-<h3>COURSES</h3>
-<h1>Browse Our Online Courses</h1>
+    <div className='bg-primary text-center h-fit grid gap-10 '>
+      <div className=' '>
+        <h3 className='text-xl text-customgreen font-semibold'>COURSES</h3>
+        <h1 className='text-4xl text-black font-semibold'>Browse Our Online Courses</h1>
 
-        </div>
-        {
-          OnlinecoursseData.map((val, i)=>{
-            return(
-              <div>
-                <div>
-                  <img src={val.icon} alt="" ></img>
-                </div>
-                <div>
-                 { val.title}
-                </div>
-                <div>
-                  {val.course}
-                </div>
+      </div>
+      {OnlinecoursseData.map((val, i) => {
+        return (
+          <div className="grid grid-cols-6 border gap-2   ">
+            <div className=' bg-white hover:bg-customgreen cursor-pointer group flex py-8 px-6 flex-col gap-5 items-center justify-center  duration-500 shadow-md'>
+              <div className='h-16 w-16 bg-mainbg'>
+                <img src={val.icon} alt="" ></img>
               </div>
-            )
-          })
-        }
+              <div className='text-md group-hover:text-white font-semibold text-black "'>
+                {val.title}
+              </div>
+              <div className="text-customgreen group-hover:text-white">
+                {val.course}
+              </div>
+            </div>
+          </div>
+        )
+      })
+
+      }
     </div>
   )
 }
