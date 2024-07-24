d834a407778bd0be78292aaadfb4a6588293d84b
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-24 06:49:04 +0545

    hello

--- src/component/pricing/pricing.jsx
+++ src/component/pricing/pricing.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
 
 const Pricing = () => {
-    const Pricingdata =[
-        {
-            plan: "",
-            price: "",
-            about: "",
-            start: "",
-        }
-    ]
+  const PricingData = [
+    {
+      plan: "BASIC PLAN",
+      price: "",
+      about: "",
+      start: "",
+    }
+  ]
   return (
-    <div>
+    <div className='bg-white text-center h-screen mx-auto gap-10 p-16'>
       <div className=''>
-          <h3 className='font-semibold text-customgreen'>OUR PRICING</h3>
-          <h1 className='text-5xl font-semibold mt-5'>Pricing & Packages</h1>
-        </div>
+        <h3 className='font-semibold text-customgreen'>OUR PRICING</h3>
+        <h1 className='text-5xl font-semibold mt-5'>Pricing & Packages</h1>
+      </div>
+      <div>
+        {PricingData.map((val, i) => {
+          return (
+            <div>
+              <div>
+                <h1>
+                  {val.plan}
+                </h1>
+              </div>
+            </div>
+
+          )
+
+
+        })
+
+        }
+      </div>
     </div>
   )
 }
