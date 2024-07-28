5abe75d4fad8886113936d2a2332e8742284c28d
Abeen Khanal <abeenkhanal16@gmail.com>
2024-07-28 14:59:01 +0545

    about

--- src/component/pages/about.jsx
+++ src/component/pages/about.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='  relative '>
+   
       
-        <div className='mx-auto w-10/12'>
-        hekki
+        <div>
+        
         </div>
-        </div>
+       
   )
 }
 
