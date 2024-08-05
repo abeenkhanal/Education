6180b532a0a16258cfb2b15302d0561da37bd115
Abeen Khanal <abeenkhanal16@gmail.com>
2024-08-05 10:24:48 +0545

    ncommented

--- src/component/pages/contact.jsx
+++ src/component/pages/contact.jsx
@@ -5,30 +5,30 @@ const Contact = () => {
     
     <div className='bg-white h-fit  mx-auto'>
       <div className='h-full w-full flex bg-gray-300 justify-center'>
-          <img src="/about.png" className='max-h-[80vh] md:max-h-[60vh] lg:max-h-[80vh]' alt="" />
+          <img src="/about.png" className='max-h-[80vh] md:max-h-[60vh] p-2 lg:max-h-[80vh]' alt="" />
         </div>
-    <div className="flex flex-col md:flex-row border-red-500 md:h-[700px] w-10/12 mx-auto ">
+    <div className="flex flex-col md:flex-row border-red-500 md:h-[800px] w-10/12 mx-auto ">
       
-      <div className="flex flex-col items-center justify-center w-1/2  bg-gray-200">
+      <div className="flex flex-col items-center justify-center lg:h-[90vh] lg:w-[100vh] lg:mt-10 md:w-[500vh] md:h-[60vh] md:flex md:mt-16 bg-gray-200">
         
           <img  className='h-full w-full' src="./contact.jpg" alt="" />
        
       </div>
 
       
-      <div className="flex flex-col justify-center w-full  p-8">
-        <h2 className="text-2xl font-bold mb-4">Contact us</h2>
+      <div className="flex flex-col justify-center w-full md:p-7 md:mb-16 md:mt-6 lg:mt-0">
+        <h2 className="text-2xl font-bold md:mt-10 mb-4 ">Contact us</h2>
         <p className="mb-4">We're open for any suggestion or just to have a chat</p>
-        <div className='flex justify-between'>
-        <div className="mb-4 w-1/4">
+        <div className='flex flex-col md:flex-row justify-between'>
+        <div className="mb-4 md:mb-0 w-full md:w-1/4">
           <p className="font-bold">ADDRESS:</p>
           <p>198 West 21th Street, Suite 721 New York NY 10016</p>
         </div>
-        <div className="mb-4">
+        <div className="mb-4 md:mb-0 w-full md:w-1/2">
           <p className="font-bold">EMAIL:</p>
           <p>info@yoursite.com</p>
         </div>
-        <div className="mb-8">
+        <div className="mb-8 md:mb-0 w-full md:w-1/3">
           <p className="font-bold">PHONE:</p>
           <p>+ 1235 2355 98</p>
         </div>
@@ -50,7 +50,7 @@ const Contact = () => {
         </form>
         <div className="mt-8">
           <p className="font-bold mb-2">Follow us here</p>
-          <div className="flex space-x-4">
+          <div className="flex space-x-4 ">
             <a href="#" className="text-teal-500">Facebook</a>
             <a href="#" className="text-teal-500">Twitter</a>
             <a href="#" className="text-teal-500">Instagram</a>
