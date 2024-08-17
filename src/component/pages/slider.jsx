cf4098370c1052263aec8f57bd8e2194d9b04efe
Abeen Khanal <abeenkhanal16@gmail.com>
2024-08-17 21:50:27 +0545

    ehl

--- src/component/pages/slider.jsx
+++ src/component/pages/slider.jsx
@@ -90,7 +90,7 @@ const Slider = () => {
 
             })}
 
-            
+    
             <div className='flex gap-32  absolute top-0 left-0 right-0 bottom-0 h-full w-full items-center   z-50 justify-between  '>
                 <button  onClick={() => {
                     if (index == 0) {
